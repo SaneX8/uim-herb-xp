433e91de214cfe33b3b177a66d34fefc545cde21
Santeri Pikkarainen <31431779+SaneX8@users.noreply.github.com>
2026-03-11 14:02:16 +0200

    Cleanup config file

---
+++ src/main/java/com/example/UimHerbXpConfig.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("uimherbxp")
+public interface UimHerbXpConfig extends Config
+{
+
+}
